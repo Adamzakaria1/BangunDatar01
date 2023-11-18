91a485fc065c616f1ecd69ff123e27119abc0087
Adamzakaria1 <150983762+Adamzakaria1@users.noreply.github.com>
2023-11-18 10:36:43 +0700

    Add files via upload

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:bangun_datar_rpl_b/main.dart';
+import 'package:bangun_datar_kelas_b/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
